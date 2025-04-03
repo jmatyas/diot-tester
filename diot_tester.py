e9d6215e5ac604f3cb867a23a74f508626650e54
jmatyas <jakubk.m@gmail.com>
2025-04-03 15:07:32 +0200

    add FTDI's serial number differentiation

--- diot_tester.py
+++ diot_tester.py
@@ -12,7 +12,6 @@ from adafruit_pca9685 import PCA9685
 from chips.mcp3221 import MCP3221
 
 
-
 # patching ftdi_mpsse.mpsse.i2c.I2C.scan method so it accepts one argument
 def patched_scan_method(self, write=False):
     return [addr for addr in range(0x78) if self._i2c.poll(addr, write)]
@@ -29,7 +28,7 @@ def make_i2c_graph(detected):
 
     for line_ix in range(0, 0x80, 0x10):
         line_contents = []
-        for addr in range(line_ix, line_ix + 0x10):   
+        for addr in range(line_ix, line_ix + 0x10):
             if addr < first or addr > last:
                 line_contents.append("  ")
             elif addr in detected:
@@ -41,10 +40,11 @@ def make_i2c_graph(detected):
 
     return buffer
 
+
 class Channel:
     def __init__(self, pwm_channel, temperature_sensor, max_power=None):
         if max_power is None:
-            max_power = 5 # 5 Watts
+            max_power = 5  # 5 Watts
         self.pwm_channel = pwm_channel
         self.temperature_sensor = temperature_sensor
         self.max_power = max_power
@@ -77,22 +77,26 @@ class Channel:
 
     @property
     def load_power(self):
-        return self.pwm_channel.duty_cycle / 0xFFFF * self.max_power # FIXME: check if this is correct
-    
+        return (
+            self.pwm_channel.duty_cycle / 0xFFFF * self.max_power
+        )  # FIXME: check if this is correct
+
     @load_power.setter
     def load_power(self, power: float):
         if power > self.max_power:
-            raise ValueError("Power exceeds maximum power of the channel")
-        
+            raise ValueError(f"Power must be less than {self.max_power} W")
+
         self.pwm_channel.duty_cycle = int(power / self.max_power * 0xFFFF)
 
-    def set_configuration(self, hysteresis: float = None, ot_shutdown: float = None, power: float = None):
+    def set_configuration(
+        self, hysteresis: float = None, ot_shutdown: float = None, power: float = None
+    ):
         if hysteresis is not None:
             self.hysteresis = hysteresis
         if ot_shutdown is not None:
             self.ot_shutdown = ot_shutdown
         if power is not None:
-            self.load_power = power   
+            self.load_power = power
 
     def report(self):
         return {
@@ -102,10 +106,15 @@ class Channel:
             "load_power": self.load_power,
         }
 
+
 class DIOTTester(I2C):
     scan_blacklist = [0x70]
 
-    def __init__(self, url="ftdi://ftdi:232h:/1", frequency=100000, ot_shutdown=80, hysteresis=75):
+    def __init__(
+        self, url="ftdi://ftdi:232h:/1", frequency=100000, ot_shutdown=80, hysteresis=75, serial=None
+    ):
+        if serial is not None:
+            url = f"ftdi://::{serial}/1"
         os.environ["BLINKA_FT232H"] = url
 
         # this is workaround; it seems that without setting the frequency explicitly
@@ -132,26 +141,26 @@ class DIOTTester(I2C):
         # Mux channels 0 and 1 swapped
         self.lm75s = [
             LM75(self.i2c_buses[1], device_address=0x48 + addr) for addr in range(8)
-        ] + [
-            LM75(self.i2c_buses[0], device_address=0x48 + addr) for addr in range(8)
-        ]
+        ] + [LM75(self.i2c_buses[0], device_address=0x48 + addr) for addr in range(8)]
 
-        self.i_monitor = MCP3221(self.i2c_buses[2], device_address=0x4D, reference_voltage=3.3)
-        self.v_monitor = MCP3221(self.i2c_buses[3], device_address=0x4D, reference_voltage=3.3)
+        self.i_monitor = MCP3221(
+            self.i2c_buses[2], device_address=0x4D, reference_voltage=3.3
+        )
+        self.v_monitor = MCP3221(
+            self.i2c_buses[3], device_address=0x4D, reference_voltage=3.3
+        )
 
         self.pwm_chips = [
             PCA9685(self.i2c_buses[2], address=0x40),
             PCA9685(self.i2c_buses[2], address=0x41),
         ]
 
-        self.aux_lm75 = LM75(self.i2c_buses[3], device_address=0x48)  # LM75 without load?
+        self.aux_lm75 = LM75(self.i2c_buses[3], device_address=0x48)
         self.aux_load = self.pwm_chips[1].channels[0]
 
         self.load_channels = [
             Channel(self.pwm_chips[0].channels[i], self.lm75s[i]) for i in range(16)
-        ] + [
-            Channel(self.aux_load, self.aux_lm75)
-        ]
+        ] + [Channel(self.aux_load, self.aux_lm75, max_power=3)]
 
         self.diot_conn_lm75s = [
             LM75(self.i2c_buses[3], device_address=0x49),  # P6 connector
@@ -164,33 +173,15 @@ class DIOTTester(I2C):
         # enable auto-increment so we can write/read registers using CP structures
         for pwm in self.pwm_chips:
             read_mode1 = pwm.mode1_reg
-            pwm.mode1_reg = read_mode1 | 0x20   
-        
+            pwm.mode1_reg = read_mode1 | 0x20
+
         for channel in self.load_channels:
             channel.set_configuration(ot_shutdown=ot_shutdown, hysteresis=hysteresis)
 
     @property
     def current(self):
-        # there is 16 resistive heaters, each around 5W from 12V -> each draws
-        # around 0.4A; additionally there is one heater for 3.3V, that draws up
-        # to 3A (around 10W) - indirectly from 12V as well.
-        # 
-        # FIXME: check if this is correct because it's against the specs
-        # 0.4A * 16 + 3A = 6.4A + 3A = 9.4A
-        # 
-        #   - issue says that the LED bar-graph should show 12V rail load up to
-        #       60W, which is 5A from 12V
-        #   - calculations around INA195 were made with the assumption of total
-        #       current of 6A.
-        # 
-        # In such case, the INA's output range would be up to around 3V 
-        # (due to R_sense=0.005 Ohm and 100 V/V gain which results in 0.5 V/A),
-        # but in case of 10A it would be up to 5V, which is unreachable for the
-        # INA monitor since it's powered from 3.3V... (moreover, the MCP3221 ADC
-        # is powered from 3.3V as well).
-        # 
-        # Let's assume that the current is around 6A, so the INA's output would
-        # be around 3V.
+        # IN195 senses current on 0.005 Ohm resistor and amplifies it by 100 V/V,
+        # which results in 0.5 V/A of 'transimpedance'.
         voltage_reading = self.i_monitor.voltage
         return voltage_reading / 0.5
 
@@ -200,18 +191,18 @@ class DIOTTester(I2C):
         # reading is 4 times lower than the actual voltage
         voltage_reading = self.v_monitor.voltage
         return voltage_reading * 4
-    
+
     def scan(self, write=False):
         # Override method from busio.i2c.scan, so it accepts one positional
         # argument
         return self._i2c.scan(write)
-    
+
     def print_i2c_tree(self):
         # on each I2C bus there is EEPROM detected. it's due to the fact, that
         # the EEPROMs are connected BEFORE the I2C MUX so, they are always detected
         # (they just respond to polling on their address)
         for ix, bus in enumerate([self, *self.i2c_buses]):
-            bus_name = "I2C Shared Bus" if ix == 0 else f"I2C Bus {ix}"           
+            bus_name = "I2C Shared Bus" if ix == 0 else f"I2C Bus {ix}"
             detected = bus.scan(write=True)
             print(f"{bus_name}:")
             print(make_i2c_graph(detected))
@@ -221,31 +212,114 @@ class DIOTTester(I2C):
             raise ValueError("Frequency must be between 24 and 1526 Hz")
         self.pwm_chips[chip_no].frequency = frequency
 
+
 if __name__ == "__main__":
-    diot_tester = DIOTTester(ot_shutdown=45, hysteresis=40)
+    diot_serials = ["DT01"]
+    diot_tester = DIOTTester(ot_shutdown=80, hysteresis=70, serial=diot_serials[0])
     eui48 = diot_tester.eeprom.eui64
     print("DIOT Tester EUI-48: 0x", "-".join([f"{x:02x}" for x in eui48]))
 
+    def measure(diot_tester):
+        i0 = diot_tester.current
+        v0 = diot_tester.voltage
+
+        print("=== Measurements with load:")
+        print("\tcurrent [A]:", i0)
+        print("\tvoltage [V]:", v0)
+        print("\tpower [W]:", i0 * v0)
+    
+    # diot_tester.print_i2c_tree()
+
+    # time.sleep(1)
+    i0 = diot_tester.current
+    v0 = diot_tester.voltage
+
     print("=== Measurements with no load:")
-    print("\tcurrent:", diot_tester.current)
-    print("\tvoltage:", diot_tester.voltage)
+    print("\tcurrent [A]:", i0)
+    print("\tvoltage [V]:", v0)
 
-    for ix, channel in enumerate(diot_tester.load_channels):
-        channel.load_power = 5
+    # # for ix, channel in enumerate(diot_tester.load_channels[:-1]):
+    # #     channel.load_power = 0
 
-    print("=== Measurements with load:")
-    print("\tcurrent:", diot_tester.current)
-    print("\tvoltage:", diot_tester.voltage)
+    # # print("=== Measurements with load:")
+    # # print("\tcurrent:", diot_tester.current)
+    # # print("\tvoltage:", diot_tester.voltage)
 
-    for i in range(10):
-        for ix, channel in enumerate(diot_tester.load_channels):
-            r = channel.report()
-            if r["temperature"] > 55:
-                print(f"OT: ch{ix}: {r['temperature']}°C... shutting down")
-                channel.load_power = 0
-            elif r["temperature"] > 35:
-                print(f"Channel{ix}: {r['temperature']}°C")
-            # channel.load_power = 5
-        print()
-        time.sleep(2.5)
+    # for i in range(10):
+    #     for ix, channel in enumerate(diot_tester.load_channels):
+    #         r = channel.report()
+    #         if r["temperature"] > 55:
+    #             print(f"OT: ch{ix}: {r['temperature']}°C... shutting down")
+    #             channel.load_power = 0
+    #         elif r["temperature"] > 35:
+    #             print(f"Channel{ix}: {r['temperature']}°C")
+    #         # channel.load_power = 5
+    #     print()
+    #     time.sleep(0.5)
 
+    set_p = []
+    ii = []
+    vv = []
+    pp = []
+    diot_tester.set_pwm_frequency(1, 1500)
+
+    for i in range(40, 55, 5):
+        lpower = i / 10
+        set_p.append(lpower)
+        for ix, channel in enumerate(diot_tester.load_channels[:5]):
+            channel.load_power = lpower
+            print(channel.report())
+        time.sleep(1)
+
+    time.sleep(5)
+
+    # for ix, channel in enumerate(diot_tester.load_channels[:-1]):
+    #     channel.load_power = 2
+
+    # for i in range(10):
+    #     measure(diot_tester)
+    #     for ix, channel in enumerate(diot_tester.load_channels[:-1]):
+    #         r = channel.report()
+    #         if r["temperature"] > 55:
+    #             print(f"OT: ch{ix}: {r['temperature']}°C... shutting down")
+    #             channel.load_power = 0
+    #         elif r["temperature"] > 35:
+    #             print(f"Channel{ix}: {r['temperature']}°C")
+    #     time.sleep(0.5)
+
+    # for i in range(0, 35, 5):
+    #     lpower = i / 10
+    #     set_p.append(lpower)
+    #     diot_tester.load_channels[-1].load_power = lpower
+    #     time.sleep(1)
+
+    #     i1 = diot_tester.current
+    #     v1 = diot_tester.voltage
+    #     ii.append(i1)
+    #     vv.append(v1)
+    #     pp.append(i1 * 3.3)
+    #     print("=== Measurements with load:")
+    #     print("\tset power [W]:", lpower)
+    #     print("\tcurrent [A]:", i1)
+    #     print("\tvoltage [V]:", v1)
+    #     print("\tpower [W]:", i1 * 3.3)
+
+    # diot_tester.load_channels[-1].load_power = 0
+
+    # with open("results.txt", "w") as f:
+    #     f.write("set_power,current,voltage,power\n")
+    #     for i in range(len(set_p)):
+    #         f.write(f"{set_p[i]},{ii[i]},{vv[i]},{pp[i]}\n")
+
+    diot_tester.load_channels[-1].load_power = 0
+    # for i in range(10):
+    #     i0 = diot_tester.current
+    #     v0 = diot_tester.voltage
+
+    #     print("=== Measurements with no load:")
+    #     print("\tcurrent [A]:", i0)
+    #     print("\tvoltage [V]:", v0)
+    #     print("\tpower [W]:", i0 * 3.3)
+
+    for ix, channel in enumerate(diot_tester.load_channels[:-1]):
+        channel.load_power = 0
--- test.py
+++ test.py
@@ -76,7 +76,7 @@ class Test(I2C):
 
 
 if __name__ == "__main__":
-    # logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.DEBUG)
     # tester = Test()
 
     # print(tester.ee.test_contents(4))
@@ -84,11 +84,139 @@ if __name__ == "__main__":
     # # def print_reg(reg):
     # #     print(f"0b{reg:08b} 0x{reg:02x}")
 
+    from pyftdi.ftdi import Ftdi
+    from pyftdi.eeprom import FtdiEeprom
+    from pyftdi.misc import hexdump
+    import sys
+    import struct
 
-    from pyftdi.i2c import I2cController
-    from pyftdi.i2c import I2cNackError
+    # EEPROM chip type (0x46, 0x56 or 0x66)
+    CHIP_TYPE = 0x46
 
-    i2c = I2cController()
-    i2c.configure("ftdi://ftdi:232h/1", frequency=50000)
-    port = i2c.get_port(0x50)
-    data = port.exchange([0x0], 1)
+    eeprom = FtdiEeprom()
+    eeprom.open('ftdi://ftdi:ft232h/1', model="93C46")
+    print(eeprom._size) # in 8 bit bytes -> 128 * 8 =1024 bits = 1Kbit
+    eeprom._chip = CHIP_TYPE
+    # print(eeprom._chip)
+    # print(eeprom.default_size)
+    # print(eeprom.size)
+
+    # NOTE: pyftd.misc.hexdump operates on 16 bit words, not on 8 bit bytes,
+    # which generates 2 times lareger output; it had to be patched   
+    print('Dumping current EEPROM config')
+    # eeprom.dump_config()
+    print(hexdump(eeprom.data, full=True))
+
+    try:
+        if eeprom.has_serial:
+            print('FTDI has SN already assigned, aborting!')
+            # sys.exit(1)
+    except AttributeError:
+        pass
+
+    eeprom.initialize()
+    # funny thing - if strings are more than 29 characters long, pyftdi throws an error
+    # that it can't fit strings into EEPROM, and the message says that it's 2 oversize
+    # characters... but when it's set to 29 characters, it works fine... (according to pyFTDI,
+    # but in reality, it produces non standard characters)... so it should be 28 characters
+    eeprom.set_manufacturer_name('WUT ISE')
+    eeprom.set_product_name('DIOT Tester v1')
+    eeprom.set_serial_number('DT01')
+    chipoff = eeprom._PROPERTIES[eeprom.device_version].chipoff
+    useroff = eeprom._PROPERTIES[eeprom.device_version].user
+
+    # https://www.ftdichip.com/Documents/AppNotes/AN_121_FTDI_Device_EEPROM_User_Area_Usage.pdf
+    # When connected to 93C46 EEPROM (128 bytes) there is one block of memory space
+    # available as user area. Size depends on the length of the Manufacturer, ManufacturerId,
+    # Description and SerialNumber strings. Max length of Manufacturer, ManufacturerId,
+    # Description and SerialNumber strings is 48 (16 bits) words. If all the 48 words are used
+    # there is no user area space left.
+    # 
+    # User area space (in bytes) = (48 - (Manufacturer + Description + ManufacturerId + SerialNumber)) * 2
+    # Start adress = the address following the last byte of SerialNumber string
+
+    print(f"Chip type: 0x{eeprom._chip:02x}")
+    print(f"EEPROM size: 0x{eeprom.default_size:02x}")
+    print(f"Device version: 0x{eeprom.device_version:02x}")
+    print(f"Chip offset: 0x{chipoff:02x}")
+    print(f"User offset: 0x{useroff:02x}")
+    print(f"size: {eeprom._size} bytes")
+
+    # size=256, user=0x1A, dynoff = 0xA0, chipoff=0x1E
+
+    eeprom._eeprom[chipoff] = CHIP_TYPE
+
+    # Bit 4 - CH A driver - if 1 VCP else D2xx
+    # Bits [3:0] - channel type
+    eeprom._eeprom[0x00] = 0x00
+    
+    # Bit 4 - Power save disable
+    # Bit 3 - FT1284 flow control
+    # Bit 2 - FT1283 DATA LSB
+    # Bit 1 - FT1284 clock IDLE state
+    eeprom._eeprom[0x01] = 0x10
+
+    # 0x02:0x03 - Vendor id
+    # 0x04:0x05 - Product id
+    # 0x06:0x07 - Device release number
+
+    # - Bit 7 - always 1
+    # - Bit 6: 1 if self powered, 0 if bus powered
+    # - Bit 5: 1 if uses remote wakeup, 0 if not
+    eeprom._eeprom[0x08] = 0xC0
+    eeprom._eeprom[0x09] = 0x96 # 150 mA
+
+
+    # Addr 0A: Chip configuration
+    # Bit 7: 0 - reserved
+    # Bit 6: 0 - reserved
+    # Bit 5: 0 - reserved
+    # Bit 4: 1 - Change USB version on BM and 2232C
+    # Bit 3: 1 - Use the serial number string
+    # Bit 2: 1 - Enable suspend pull downs for lower power
+    # Bit 1: 1 - Out EndPoint is Isochronous
+    # Bit 0: 1 - In EndPoint is Isochronous
+    eeprom._eeprom[0x0a] = 0x08
+
+
+    # # - group settings, other FT4232H dongles have 0 here
+    # eeprom._eeprom[0x0c] = 0x00 # ?
+    # eeprom._eeprom[0x0d] = 0x00 # ?
+
+    # 0x18 - 0x1D - CBUS functions
+
+    # # Manufacturing string
+    man_string_off , man_string_length = eeprom._eeprom[0x0E]-0x80, eeprom._eeprom[0x0F]
+    print(man_string_off, man_string_length) # 0xC0 - 0x80 = 0x40, 0x0A - man_string_off, man_string_length 
+    # # Product string
+    prod_string_off, prod_string_length = eeprom._eeprom[0x10]-0x80, eeprom._eeprom[0x11] 
+    print(prod_string_off, prod_string_length)
+    # # Serial number string
+    serial_string_off, serial_string_length = eeprom._eeprom[0x12]-0x80, eeprom._eeprom[0x13]
+    print(serial_string_off, serial_string_length)
+
+
+    bstream = bytearray()
+    bstream.extend("DT_SLOT_01\0".encode("utf-8"))
+    eeprom._eeprom[useroff:useroff+len(bstream)] = bstream
+
+
+    print(hexdump(eeprom.data, full=True))
+
+
+
+    eeprom._dirty.add('eeprom')
+    eeprom.sync()
+    # eeprom.dump_config()
+
+
+    # eeprom.commit(dry_run=False)
+
+
+    eeprom.close()
+
+    print(Ftdi.list_devices())
+
+
+
+    
